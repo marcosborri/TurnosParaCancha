03c27f7ba01aaa44491270639a2411c6e40f0873
Chulo096 <1lucho2cavero3@gmail.com>
2025-11-07 07:22:57 -0300

    Field integration test done

--- src/tests/integration/integrated.field.test.ts
+++ src/tests/integration/integrated.field.test.ts
@@ -37,12 +37,60 @@ describe('integration [GET] /', ()=> {
 
 describe('integration [GET] /fields/', ()=>{
 
-    test("Elements quantity should be 5", ()=>{
-        const fields = getFields(5)
-        mockedFieldModel.getFields.mockResolvedValue(fields)
+    test("Elements quantity should be 5", async ()=>{
+        const fields = await getFields(5);
+        mockedFieldModel.getFields.mockResolvedValue(fields);
 
         return request(app).get("/fields/").then(({ body })=>{
             expect(body.length).toBe(5)
         })
+    })
+})
+
+describe('integration [GET] /fields/:id', ()=>{
+    test("should return one field", async()=>{
+        const oneField = await getOneField();
+        mockedFieldModel.getField.mockResolvedValue(oneField);
+
+        const id = oneField.getId();
+
+        const { status, body } = await request(app).get(`/fields/${id}`);
+        expect(status).toBe(200)
+        expect(body).toEqual(oneField)
+    })
+})
+
+describe('integration [POST] /fields/', ()=>{
+    test("should create a test", async () =>{
+        const oneField = await getOneField();
+        mockedFieldModel.addField.mockResolvedValue(oneField);
+
+        const data = {
+            name: oneField.getName(),
+            type: oneField.getTypeField(),
+            price: oneField.getPrice()
+        };
+
+        const { status, body } = await request(app)
+            .post("/fields/")
+            .send(data);
+
+        expect(status).toBe(201);
+        expect(body.name).toBe(data.name);
+        expect(body.typeField).toBe(data.type);
+        expect(body.price).toBe(data.price);
+    })
+})
+
+describe('integration [PUT] /fields/:id/name', ()=>{
+    test("Should update the field name", async ()=>{
+        const oneField = await getOneField();
+        mockedFieldModel.editFieldName.mockResolvedValue({
+            ...oneField,
+            getName: () => "New Name"
+        });
+        const { status, body } = await request(app).put(`/fields/name`).send({ id: oneField.getId(), name: "New Name" });
+        expect(status).toBe(200);
+        expect(body.name).toBe("New Name")
     })
 })
