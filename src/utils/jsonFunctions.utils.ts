bb411661d2e3aede6dbd896039c1e5fd15d8eb2c
Chulo096 <1lucho2cavero3@gmail.com>
2025-11-23 20:28:30 -0300

    JSON functions for users done

--- src/utils/jsonFunctions.utils.ts
+++ src/utils/jsonFunctions.utils.ts
@@ -10,6 +10,16 @@ export async function saveData(data: Array<any> | any, filePath: string) {
   }
 
 export async function loadData(filePath: string) {
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, JSON.stringify([]));
+      return [];
+    }
     const fileContent = fs.readFileSync(filePath, 'utf8');
+    
+    if (!fileContent) {
+      await fs.promises.writeFile(filePath, "[]");
+      return [];
+    }
+
     return JSON.parse(fileContent);
   }
